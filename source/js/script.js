94ef387f8b774a20813ad2322a1d515bf7819506
Alex Lin <Alex_vii@live.com>
2016-07-16 02:10:54 +0800

    31st Commit
    Category-row Can Scroll Now

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
-function catRowMov () {
-	var row = document.getElementById("category-row");
-	var x = window.getComputedStyle(el, null);
-	var y = getPropertyValue("transform");
-	row.style.transform = "translate(0, 0)";
-}
+var row = document.getElementById("category-row");
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var translateX = 0;
+
+b1.addEventListener("click", function () {row.style.transform = "translateX(10%)";} , false);
+b2.addEventListener("click", function () {row.style.transform = "translate(0, 0)";} , false);
