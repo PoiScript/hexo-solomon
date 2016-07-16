3e101a106220b6839fcca47faa4eef4748429200
Alex Lin <Alex_vii@live.com>
2016-07-16 17:17:55 +0800

    33rd Commit

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,15 @@
 var row = document.getElementById("category-row");
-var b1 = document.getElementById("b1");
-var b2 = document.getElementById("b2");
+var b1 = document.getElementById("controls_prev");
+var b2 = document.getElementById("controls_next");
 var a = 0;
 
 b1.addEventListener("click", function () {
-	row.style.transform = "translateX(" + (a -1 + 11) + "%)";
-	a = row.style.transform.match("-?[0-9]+");} , false);
+	row.style.transform = "translateX(" + (a - 1 + 9) + "%)";
+	a = row.style.transform.match("-?[0-9]+");
+	if(a == 0){b1.style.visibility = "hidden"};
+	if(a > -32){b2.style.visibility = "visible"}} , false);
 b2.addEventListener("click", function () {
-	row.style.transform = "translateX(" + (a - 10) + "%)";
-	a = row.style.transform.match("-?[0-9]+");} , false);
+	row.style.transform = "translateX(" + (a - 8) + "%)";
+	a = row.style.transform.match("-?[0-9]+");
+	if(a < 0) {b1.style.visibility = "visible"};
+	if(a == -32){b2.style.visibility = "hidden"}} , false);
