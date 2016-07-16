5e3be15c4392f4330911d5c5cfbe87fe47c33742
Alex Lin <Alex_vii@live.com>
2016-07-17 01:45:30 +0800

    35th Commit

--- source/js/script.js
+++ source/js/script.js
@@ -4,12 +4,12 @@ var b2 = document.getElementById("controls_next");
 var a = 0;
 
 b1.addEventListener("click", function () {
-	row.style.transform = "translateX(" + (a - 1 + 9) + "%)";
+	row.style.transform = "translateX(" + (a - 1 + 17) + "%)";
 	a = row.style.transform.match("-?[0-9]+");
 	if(a == 0){b1.style.visibility = "hidden"};
 	if(a > -32){b2.style.visibility = "visible"}} , false);
 b2.addEventListener("click", function () {
-	row.style.transform = "translateX(" + (a - 8) + "%)";
+	row.style.transform = "translateX(" + (a - 16) + "%)";
 	a = row.style.transform.match("-?[0-9]+");
 	if(a < 0) {b1.style.visibility = "visible"};
 	if(a == -32){b2.style.visibility = "hidden"}} , false);
