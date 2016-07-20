eb49c030b7128d8697365086ea9d14b1b3d8224e
Alex Lin <Alex_vii@live.com>
2016-07-20 23:46:46 +0800

    38th Commit

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,16 @@ b2.addEventListener("click", function () {
 	a = row.style.transform.match("-?[0-9]+");
 	if(a < 0) {b1.style.visibility = "visible"};
 	if(a == -32){b2.style.visibility = "hidden"}} , false);
+
+var links = new Array();
+for (var i = site.posts.length - 1; i >= 0; i--) {
+	links[i] = site.posts[i].path;
+}
+
+function openLink() {
+  // Chooses a random link:
+  var i = Math.floor(Math.random() * links.length);
+  // Directs the browser to the chosen target:
+  parent.location = links[i];
+  return false;
+}
