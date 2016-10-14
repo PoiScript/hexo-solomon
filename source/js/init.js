e32afe26245e9eebbd81480bc15e4c4e8cbf3efe
PoiScript <poiscript@gmail.com>
2016-10-14 23:29:58 +0800

    add flax feature

--- source/js/init.js
+++ source/js/init.js
@@ -5,5 +5,11 @@ $(document).ready(function(){
   $('.slider').slider()
 })
 $(document).ready(function(){
+  $('.toc-wrapper').pushpin({top: $('.toc-wrapper').offset().top})
   $('.scrollspy').scrollSpy()
+})
+$(document).ready(function () {
+  $('#plax-front').plaxify({"xRange":40,"yRange":40})
+  $('#plax-back').plaxify({"xRange":10,"yRange":10})
+  $.plax.enable()
 })
